25d6da87fcbf862209dace8877633b7722067588
Angelos Ioannou <t704@FRA-1.local>
2018-06-19 14:21:59 +0200

    changed port

--- main.js
+++ main.js
@@ -21,6 +21,6 @@ io.on("connection",function(socket){
 
 });
 
-http.listen(3000,function(){
-    console.log("Listening on 3000");
+http.listen(8080,function(){
+    console.log("Listening on 8080");
 });
