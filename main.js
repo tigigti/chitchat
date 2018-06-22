46b98c51aeb0a6d07f2201a43df706d2da513255
Angelos Ioannou <t704@FRA-1.local>
2018-06-22 15:05:03 +0200

    modal window and js logic

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+
+// To serve css and other static files
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
